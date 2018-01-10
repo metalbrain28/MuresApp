49ed12e1f0d271708453f90580360dcedfa8b5a7
Andreea Dobroteanu <andreea.dobroteanu@gmail.com>
2018-01-10 22:55:47 +0200

    Added db file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mustacheExpress = require('mustache-express');
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('../river.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('river.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
     }
