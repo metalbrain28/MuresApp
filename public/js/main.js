484f8d2aa8e7848a4fed3daa15b51e95d6a4a878
MadalinaMitroi <madamitroi@yahoo.ro>
2018-01-15 00:23:46 +0200

    Added center and limits

--- public/js/main.js
+++ public/js/main.js
@@ -50,7 +50,11 @@ require([
 
         //if accessing webmap from a portal outside of ArcGIS Online, uncomment and replace path with portal URL
         //arcgisUtils.arcgisUrl = "https://pathto/portal/sharing/content/items";
-        arcgisUtils.createMap("9526bb9b57654adbb98536004283a80c", "map").then(function (response) {
+        arcgisUtils.createMap("a21aac6bafac4a8db22eb167822f7cc8", "map", {
+            mapOptions: {
+                center: [21.655, 46.075],
+                zoom:9
+            }}).then(function (response) {
             //update the app
             dom.byId("title").innerHTML = response.itemInfo.item.title;
             dom.byId("subtitle").innerHTML = response.itemInfo.item.snippet;
@@ -88,6 +92,17 @@ require([
         );
 
         function handleMapExtraActions(map) {
+
+            var myPoint = new Point(21.108430175781148, 46.27374974057721);
+            var symbol0 = new SimpleMarkerSymbol().setColor(new Color('blue'));
+            var graphic = new Graphic(myPoint, symbol0);
+            map.graphics.add(graphic);
+
+            myPoint = new Point(22.352631835936794, 46.00922633069789);
+            symbol0 = new SimpleMarkerSymbol().setColor(new Color('blue'));
+            graphic = new Graphic(myPoint, symbol0);
+            map.graphics.add(graphic);
+
             map.on("click", function(evt){
 
                 if (!isAuthenticated()) return;
@@ -96,14 +111,25 @@ require([
                 longitude = evt.mapPoint.getLongitude();
                 point = new Graphic(evt.mapPoint, symbol);
 
-
                 /* For checking not to interfere with existing points */
                 var layerCircles = $(".esriMapContainer circle");
                 var isInFirstLayer = layerCircles.filter(function(k, v) {return v === evt.target;}).length;
                 if (!isInFirstLayer) {
+
+                    /*var ok3 = false;
+                    for (i = 0; i < issues.length; i++) {
+                        if((issues[i].geometry.getLatitude() <= (latitude + 0.005) && issues[i].geometry.getLatitude() >= (latitude - 0.005))
+                            && (issues[i].geometry.getLongitude() <= (longitude + 0.005) && issues[i].geometry.getLongitude() >= (longitude - 0.005))) {
+                            ok3 = true;
+                            break;
+                        }
+                    }
+                    var form = null;
+                    if (ok3 === true)
+                        form = "<b>Latitude: </b>" + latitude + "<br><br> <b>Longitude: </b>" + longitude + "<br><br>  <b>Issue Description: </b>" + description + "</b>";*/
                     map.graphics.add(point);
 
-                    var form = "<b>Latitude: </b>" + latitude + "<br><br> <b>Longitude: </b>" + longitude + "<br><br><form id='add_point'> Describe issue:<br><input type=" + "'text'" + "class='add_issue'" + "name=" + "'describe'" + "><br><br><input type=" + "'submit'" + " class='submit-incident' value=" + "'Submit'" + "></form> ";
+                    form = "<b>Latitude: </b>" + latitude + "<br><br> <b>Longitude: </b>" + longitude + "<br><br><form id='add_point'> Describe issue:<br><input type=" + "'text'" + "class='add_issue'" + "name=" + "'describe'" + "><br><br><input type=" + "'submit'" + " class='submit-incident' value=" + "'Submit'" + "></form> ";
 
                     map.infoWindow.setContent(form);
                     map.infoWindow.show(evt.mapPoint);
@@ -119,7 +145,7 @@ require([
             e.preventDefault();
 
             description = $('.add_issue').val();
-            submitted = true;
+            //submitted = true;
             issues.push(point);
 
             request.post("/incidents", {
