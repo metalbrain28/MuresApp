bdc3cffc5ff4b1219325cd4136ac189b93e50965
MadalinaMitroi <madamitroi@yahoo.ro>
2018-01-15 23:00:22 +0200

    Added info in popups

--- public/js/main.js
+++ public/js/main.js
@@ -45,13 +45,13 @@ require([
             point = null;
         var issues = [];
         let incidents = [];
-
+        var limits = [];
 
         parser.parse();
 
         //if accessing webmap from a portal outside of ArcGIS Online, uncomment and replace path with portal URL
         //arcgisUtils.arcgisUrl = "https://pathto/portal/sharing/content/items";
-        arcgisUtils.createMap("a21aac6bafac4a8db22eb167822f7cc8", "map", {
+        arcgisUtils.createMap("353ec0f534254837bc775fe0d20c16db", "map", {
             mapOptions: {
                 center: [21.655, 46.075],
                 zoom:9
@@ -98,11 +98,13 @@ require([
             var symbol0 = new SimpleMarkerSymbol().setColor(new Color('blue'));
             var graphic = new Graphic(myPoint, symbol0);
             map.graphics.add(graphic);
+            limits.push({name: "Semlac", longitude: 21.108430175781148, latitude: 46.27374974057721});
 
             myPoint = new Point(22.352631835936794, 46.00922633069789);
             symbol0 = new SimpleMarkerSymbol().setColor(new Color('blue'));
             graphic = new Graphic(myPoint, symbol0);
             map.graphics.add(graphic);
+            limits.push({name: "Petris", longitude: 22.352631835936794, latitude: 46.00922633069789});
 
             map.on("click", function(evt){
 
@@ -117,21 +119,9 @@ require([
                 var layerCircles = $(".esriMapContainer circle");
                 var isInFirstLayer = layerCircles.filter(function(k, v) {return v === evt.target;}).length;
                 if (!isInFirstLayer) {
-
-                    /*var ok3 = false;
-                    for (i = 0; i < issues.length; i++) {
-                        if((issues[i].geometry.getLatitude() <= (latitude + 0.005) && issues[i].geometry.getLatitude() >= (latitude - 0.005))
-                            && (issues[i].geometry.getLongitude() <= (longitude + 0.005) && issues[i].geometry.getLongitude() >= (longitude - 0.005))) {
-                            ok3 = true;
-                            break;
-                        }
-                    }
-                    var form = null;
-                    if (ok3 === true)
-                        form = "<b>Latitude: </b>" + latitude + "<br><br> <b>Longitude: </b>" + longitude + "<br><br>  <b>Issue Description: </b>" + description + "</b>";*/
                     map.graphics.add(point);
 
-                    form = "<b>Latitude: </b>" + latitude + "<br><br> <b>Longitude: </b>" + longitude + "<br><br><form id='add_point'> Describe issue:<br><input type=" + "'text'" + "class='add_issue'" + "name=" + "'describe'" + "><br><br><input type=" + "'submit'" + " class='submit-incident' value=" + "'Submit'" + "></form> ";
+                    var form = "<b>Latitude: </b>" + latitude + "<br><br> <b>Longitude: </b>" + longitude + "<br><br><form id='add_point'> Describe issue:<br><input type=" + "'text'" + "class='add_issue'" + "name=" + "'describe'" + "><br><br><input type=" + "'submit'" + " class='submit-incident' value=" + "'Submit'" + "></form> ";
 
                     map.infoWindow.setContent(form);
                     map.infoWindow.show(evt.mapPoint);
@@ -139,6 +129,41 @@ require([
                     if(!submitted)
                         removeMapClickBullet();
                     submitted = false;
+                }
+
+
+                var customLayer = $("#map_graphics_layer circle");
+                var isInCustomLayer = customLayer.filter(function(k, v) {return v === evt.target;}).length;
+                if (isInCustomLayer) {
+                    var ok = false;
+                    var i = 0;
+                    for (i = 0; i < incidents.length; i++) {
+                        if((incidents[i].Latitude <= (latitude + 0.013) && incidents[i].Latitude >= (latitude - 0.013))
+                            && (incidents[i].Longitude <= (longitude + 0.013) && incidents[i].Longitude >= (longitude - 0.013))) {
+                            ok = true;
+                            break;
+                        }
+                    }
+
+                    var form = null;
+                    if (ok === true) {
+                        form = "<b>Latitude: </b>" + incidents[i].Latitude + "<br><br> <b>Longitude: </b>" + incidents[i].Longitude + "<br><br>  <b>Reporter: </b>" + incidents[i].Name + "</b>" + "<br><br>  <b>Issue Description: </b>" + incidents[i].Description + "</b>";
+                    } else if((limits[0].latitude <= (latitude + 0.013) && limits[0].latitude >= (latitude - 0.013))
+                        && (limits[0].longitude <= (longitude + 0.013) && limits[0].longitude >= (longitude - 0.013)))
+                    {
+                        form = "<b>Latitude: </b>" + limits[0].latitude + "<br><br> <b>Longitude: </b>" + limits[0].longitude + "<br><br>  <b><u>Location:</u> </b><i>" + limits[0].name + "</i></b>";
+                    } else if((limits[1].latitude <= (latitude + 0.013) && limits[1].latitude >= (latitude - 0.013))
+                        && (limits[1].longitude <= (longitude + 0.013) && limits[1].longitude >= (longitude - 0.013)))
+                    {
+                        form = "<b>Latitude: </b>" + limits[1].latitude + "<br><br> <b>Longitude: </b>" + limits[1].longitude + "<br><br>  <b><u>Location:</u> </b><i>" + limits[1].name + "</i></b>";
+                    }
+
+                    map.infoWindow.setContent(form);
+                    map.infoWindow.show(evt.mapPoint);
+
+                  //  if(!submitted)
+                        removeMapClickBullet();
+                   // submitted = false;
                 }
             });
         }
